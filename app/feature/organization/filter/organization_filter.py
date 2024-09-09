be72417c2e47c4027d2b364dc05f40a3a899f43f
IDLKZ <idlkz2019@gmail.com>
2024-09-09 17:25:04 +0500

    Added Orgnization Logic

---
+++ app/feature/organization/filter/organization_filter.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+from fastapi import Query
+from sqlalchemy import or_, and_
+
+from app.domain.models.organization_model import OrganizationModel
+
+
+class OrganizationFilter(BaseFilter):
+    def __init__(self,
+                 per_page: int = Query(default=20, gt=0, example=20, description="Количество элементов на страницу"),
+                 page: int = Query(default=1, ge=1, example=1, description="Номер страницы"),
+                 search: Optional[str] = Query(default=None, max_length=255, min_length=3,
+                                               description="Поисковый запрос по имени, телефону, почте, иину"),
+                 role_id: Optional[int] = Query(default=None, gt=0, description="Введите роль пользователя"),
+                 type_id: Optional[int] = Query(default=None, gt=0, description="Введите тип пользователя")):
+        super().__init__(per_page, page, search)
+        self.per_page = per_page
+        self.page = page
+        self.search = search
+        self.role_id = role_id
+        self.type_id = type_id
+        self.model = OrganizationModel
+
+    def apply(self) -> list:
+        filters = []
+        if (self.search):
+            filters.append(or_(
+                self.model.email.like(f"%{self.search}%"),
+                self.model.phone.like(f"%{self.search}%"),
+                self.model.bin.like(f"%{self.search}%"),
+                self.model.bik.like(f"%{self.search}%"),
+                self.model.kbe.like(f"%{self.search}%"),
+                self.model.full_name.like(f"%{self.search}%"),
+                self.model.short_name.like(f"%{self.search}%"),
+                self.model.address.like(f"%{self.search}%"),
+                )
+            )
+        if (self.role_id):
+            filters.append(and_(self.model.role_id == self.role_id))
+        if (self.type_id):
+            filters.append(and_(self.model.type_id == self.type_id))
+        return filters
